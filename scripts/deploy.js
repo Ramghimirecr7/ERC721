032bb54544b23727c0365ffef6b6ba293b627c58
Ram Ghimire <ramghimire@Rams-MacBook-Pro.local>
2022-06-11 02:28:10 -0500

    inital commit

---
+++ scripts/deploy.js
@@ -0,0 +1,20 @@
+//import ether from hardhat package
+const { ethers } = require("hardhat");
+
+async function main(){
+
+    const nftContract = await ethers.getContractFactory("GameItem");
+
+  // here we deploy the contract
+  const deployedNFTContract = await nftContract.deploy();
+
+  // print the address of the deployed contract
+  console.log("NFT Contract Address:", deployedNFTContract.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
